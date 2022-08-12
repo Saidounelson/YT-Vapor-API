2ae0e5fe3b4e06741fa451b50d8facf8a42d7e32
saidou Nassufou <saidounelson@gmail.com>
2022-08-12 12:17:43 +0000

    update file

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -6,27 +6,26 @@ import Vapor
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-    if let databaseURL = Environment.get("DATABASE_URL"),
+   
+    /*if let databaseURL = Environment.get("DATABASE_URL"),
        var postgresConfig = PostgresConfiguration(url: databaseURL) {
         var tlsConfig = TLSConfiguration.makeClientConfiguration()
         tlsConfig.certificateVerification = .none
         postgresConfig.tlsConfiguration=tlsConfig
         app.databases.use(.postgres(configuration: postgresConfig), as: .psql)
-    }else {
-        app.databases.use(.postgres(
-            hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-            port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
-            username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-            password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-            database: Environment.get("DATABASE_NAME") ?? "vapor_database"
-        ), as: .psql)
-    }
+    }else {}
+    */
+    app.databases.use(.postgres(
+        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
+        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
+    ), as: .psql)
     
     app.migrations.add(CreateSongs())
-    
-    if app.environment == .development{
-        try app.autoMigrate().wait()
-    }
+    try app.autoMigrate().wait()
+    // if app.environment == .development{}
     // register routes
     try routes(app)
 }
